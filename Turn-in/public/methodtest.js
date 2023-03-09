f930c7a1db0c46eacef92ce840548182ee0345f1
Kenny <kennychan09870@gmail.com>
2023-03-08 16:56:42 -0800

    modulized code

---
+++ Turn-in/public/methodtest.js
@@ -0,0 +1,45 @@
+import responseHandler from "../modules/response-handler.js"
+
+const postHandler = {
+  doc: document.getElementById("post-btn"),
+  handleClick() {
+    this.doc.addEventListener("click", (e) => {
+      e.preventDefault();
+      responseHandler.makeRequest("https://httpbin.org/post", "POST");
+    });
+  },
+};
+
+const getHandler = {
+  doc: document.getElementById("get-btn"),
+  handleClick() {
+    this.doc.addEventListener("click", (e) => {
+      e.preventDefault();
+      responseHandler.makeRequest("https://httpbin.org/get", "GET");
+    });
+  },
+};
+
+const putHandler = {
+  doc: document.getElementById("put-btn"),
+  handleClick() {
+    this.doc.addEventListener("click", (e) => {
+      e.preventDefault();
+      responseHandler.makeRequest("https://httpbin.org/put", "PUT");
+    });
+  },
+};
+const deleteHandler = {
+  doc: document.getElementById("delete-btn"),
+  handleClick() {
+    this.doc.addEventListener("click", (e) => {
+      e.preventDefault();
+      responseHandler.makeRequest("https://httpbin.org/delete", "delete");
+    });
+  },
+};
+
+postHandler.handleClick();
+getHandler.handleClick();
+putHandler.handleClick();
+deleteHandler.handleClick();
